4604953b9698c9dfa6874a9389bebe16c5ce73bb
Marcin Pekalski <mpekalski@gmail.com>
2019-07-09 23:00:55 +0200

    3.7

--- setup.py
+++ setup.py
@@ -179,6 +179,7 @@ def main():
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
           'Programming Language :: C++',
           'Programming Language :: Python :: Implementation :: CPython',
           'Topic :: Scientific/Engineering',
